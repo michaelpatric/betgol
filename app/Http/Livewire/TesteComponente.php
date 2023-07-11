635863c3ab54e0eca24cf8068ad65784b98e4e27
Michael Favero <56142976+michaelpatric@users.noreply.github.com>
2023-07-11 00:12:52 -0300

    mais um commit

---
+++ app/Http/Livewire/TesteComponente.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class TesteComponente extends Component
+{
+    public function render()
+    {
+        return view('livewire.teste-componente');
+    }
+}
