aeff9de29e3864d708e4a527c9640c23c30ab63d
Michael Favero <56142976+michaelpatric@users.noreply.github.com>
2023-07-16 01:31:07 -0300

    novo commit

--- resources/views/pagina01.blade.php
+++ resources/views/pagina01.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Nova pagina</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -17,6 +17,8 @@
 </head>
 <body class="antialiased">
 <h1>Oláaaaaaaaaa</h1>
+
+<p>Fazendo um novo paragrafo</p>
 
 </body>
 </html>
